ceb0149d9c037af89e9c86480685ecc82f0ebe9c
adamstickley <12855940+adamstickley@users.noreply.github.com>
2025-05-23 12:52:26 +1000

    third test

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ import { TestEntity } from './entity/test.entity';
 export class AppService {
   constructor(private parentRepo: ParentRepository) {}
 
-  async run(): Promise<ParentEntity> {
+  async run(options?: { withNoRefresh: boolean }): Promise<ParentEntity> {
     const newChild = new TestEntity();
     newChild.title = 'child 1';
     newChild.richTextDescription = { json: { example: 1 } };
@@ -18,8 +18,10 @@ export class AppService {
     // create
     await this.parentRepo.getEntityManager().persistAndFlush(example);
 
-    // refresh; ensuring any remote changes that might have happened in the DB
-    await this.parentRepo.getEntityManager().refresh(example);
+    if (!options?.withNoRefresh) {
+      // refresh; ensuring any remote changes that might have happened in the DB
+      await this.parentRepo.getEntityManager().refresh(example);
+    }
 
     example.title = 'new title';
     // only updating title; should expect only 1 update call: to parent entity
